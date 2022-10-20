8d8af1c3d4b13bfbc5a8b66abd974d7707b1b210
Ricardo Costa <ricardo_hentges@hotmail.com>
2022-10-20 13:44:26 -0300

    Criado o enum de categoria

---
+++ Categoria.java
@@ -0,0 +1,11 @@
+public enum Categoria {
+    
+    LEITO("Leito"), EXECUTIVO("Executivo"), CONVENCIONAL("Convencional");
+
+    public String categoria;
+
+    private Categoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
