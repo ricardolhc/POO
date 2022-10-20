c8cb234764bcbfbf1d22070889dc234ade4a0a4c
Ricardo Costa <ricardo_hentges@hotmail.com>
2022-10-20 14:38:28 -0300

    retirado o construtor secundario, arrumado o toString

--- Veiculo.java
+++ Veiculo.java
@@ -10,35 +10,22 @@ public abstract class Veiculo {
         this.diaria = diaria;
     }
 
-    public Veiculo(String placa, double diaria) {
-        this(placa, 0, diaria);
-    }
-
-    public Veiculo(String placa, int ano) {
-        this(placa, ano, 0);
-    }
-
-    public Veiculo(String placa) {
-        this(placa, 0, 0);
-    }
-
     public String getPlaca() { return placa; }
 
     public int getAno() { return ano; }
 
     public double getDiaria() { return diaria; }
 
+    public void setPlaca(String placa) { this.placa = placa; }
 
+    public void setAno(int ano) { this.ano = ano; }
 
-    public void setPlaca(String placa){ this.placa = placa; }
-
-    public void setAno(int ano){ this.ano = ano; }
-
-    public void setDiaria(double diaria){ this.diaria = diaria; }
+    public void setDiaria(double diaria) { this.diaria = diaria; }
 
     public String toString() {
-        return "Placa: " + placa + " Ano: " + ano + " Diaria: " + diaria;
+        return " Placa: " + placa + 
+               " Ano: " + ano + 
+               " Diaria: " + diaria;
     }
-    
     
 }
